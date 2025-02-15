04b047f410ce5fd0ba6d0b6f007ef78eef48981a
Junaid Atari <mj.atari@gmail.com>
2025-02-15 22:07:30 +0500

    refactor(core): initialize controllers, database configuration

--- src/Movies.Api/Program.cs
+++ src/Movies.Api/Program.cs
@@ -1,37 +1,34 @@
+using Movies.Application;
+using Movies.Application.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var config = builder.Configuration;
+
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddApplication();
+builder.Services.AddDatabase(config["Database:ConnectionString"]!);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
-  app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
-var summaries = new[] {
-  "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
+await dbInitializer.InitializeAsync();
 
-app.MapGet("/weatherforecast", () => {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-          DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-          Random.Shared.Next(-20, 55),
-          summaries[Random.Shared.Next(summaries.Length)]
-        ))
-      .ToArray();
-    return forecast;
-  })
-  .WithName("GetWeatherForecast");
+/*app.MapGet("/test", () => {
+  var result = new {
+    Success = true,
+  };
+  return result;
+});*/
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
-  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

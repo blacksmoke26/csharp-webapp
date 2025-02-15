45b836623c9b25d668cefbdf8c2678acb5a44d54
Junaid Atari <mj.atari@gmail.com>
2025-02-15 21:55:47 +0500

    feat(m): Add `Slug` property and method to generate slug

--- src/Movies.Application/Models/Movie.cs
+++ src/Movies.Application/Models/Movie.cs
@@ -2,6 +2,8 @@
 // Copyright (c) 2025 Junaid Atari, and contributors
 // Website: https://github.com/blacksmoke26/
 
+using CaseConverter;
+
 namespace Movies.Application.Models;
 
 public class Movie {
@@ -9,8 +11,14 @@ public class Movie {
 
   // section Test
   public required string Title { get; set; }
-  
+
   public required int YearOfRelease { get; set; }
-  
+
+  public string Slug => GenerateSlug();
+
   public required List<string> Genres { get; init; } = [];
+
+  private string GenerateSlug() {
+    return $"{Title} {YearOfRelease}".ToKebabCase();
+  }
 }

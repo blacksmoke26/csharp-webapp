b2fb7cd207500d422458e1e6a63f44138dd7ec87
Junaid Atari <mj.atari@gmail.com>
2025-02-16 22:36:02 +0500

    refactor(type): change property type to nullable-long

--- src/Movies.Contracts/Responses/MovieResponse.cs
+++ src/Movies.Contracts/Responses/MovieResponse.cs
@@ -1,7 +1,7 @@
 namespace Movies.Contracts.Responses;
 
 public class MovieResponse {
-  public required Guid Id { get; init; }
+  public long? Id { get; init; }
   
   public required string Title { get; init; }
   

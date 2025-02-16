0941b751f1cc00b9b628586df1338c1e0832b08a
Junaid Atari <mj.atari@gmail.com>
2025-02-16 22:30:54 +0500

    feat(sql): create new table 'genres' along with the indexes

--- src/Movies.Application/Database/DbInitializer.cs
+++ src/Movies.Application/Database/DbInitializer.cs
@@ -11,13 +11,13 @@ public class DbInitializer(IDbConnectionFactory connectionFactory) {
 
   public async Task InitializeAsync() {
     using var connection = await _connectionFactory.CreateConnectionAsync();
-    await connection.ExecuteAsync("""
+    await connection.ExecuteAsync(
+      """
       CREATE TABLE IF NOT EXISTS "public"."movies" (
         "id" serial8 NOT NULL,
         "title" varchar(60) NOT NULL,
         "yearOfRelease" integer NOT NULL,
         "slug" varchar(255),
-        "genres" varchar[],
         PRIMARY KEY ("id"),
         CONSTRAINT "UNQ_Title_YoR" UNIQUE ("title", "yearOfRelease"),
         CONSTRAINT "UNQ_slug" UNIQUE ("slug")
@@ -28,20 +28,31 @@ public class DbInitializer(IDbConnectionFactory connectionFactory) {
       );
 
       COMMENT ON COLUMN "public"."movies"."id" IS 'ID';
-
       COMMENT ON COLUMN "public"."movies"."title" IS 'Title';
-
       COMMENT ON COLUMN "public"."movies"."yearOfRelease" IS 'Year of Release';
-
       COMMENT ON COLUMN "public"."movies"."slug" IS 'Slug';
-
-      COMMENT ON COLUMN "public"."movies"."genres" IS 'Genres';
-
-      COMMENT ON CONSTRAINT "UNQ_Title_YoR" ON "public"."movies" IS 'Title and Year should be unique';
-
+      COMMENT ON CONSTRAINT "UNQ_Title_YoR" ON "public"."movies" IS 'Title and Year must be unique';
       COMMENT ON CONSTRAINT "UNQ_slug" ON "public"."movies" IS 'Slug is always unique (alternative to ID)';
+      COMMENT ON INDEX "public"."IDX_title" IS 'Index needed due to search between titles';
+      """);
 
-      COMMENT ON INDEX "public"."IDX_title" IS 'Index needed due to search in titles';
+    await connection.ExecuteAsync(
+      """
+        CREATE TABLE IF NOT EXISTS "public"."genres" (
+        "id" serial8 NOT NULL,
+        "movieId" int8 NOT NULL,
+        "name" varchar(60) NOT NULL,
+        PRIMARY KEY ("id"),
+        CONSTRAINT "FK_movies_id_genres_movie_id" FOREIGN KEY ("movieId") 
+          REFERENCES "public"."movies" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+        CONSTRAINT "UNQ_movie_name_genre" UNIQUE ("movieId", "name")
+      );
+
+      COMMENT ON COLUMN "public"."genres"."id" IS 'ID';
+      COMMENT ON COLUMN "public"."genres"."movieId" IS 'Movie ID';
+      COMMENT ON COLUMN "public"."genres"."name" IS 'Name';
+      COMMENT ON CONSTRAINT "FK_movies_id_genres_movie_id" ON "public"."genres" IS 'Reference to Movie ID';
+      COMMENT ON CONSTRAINT "UNQ_movie_name_genre" ON "public"."genres" IS 'Movie ID and Genre name must be unique';
       """);
   }
 }

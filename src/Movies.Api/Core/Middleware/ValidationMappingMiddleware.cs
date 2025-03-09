73881a9aacc4d52f3ceffa4c5739d73564b5c53a
Junaid Atari <mj.atari@gmail.com>
2025-03-09 16:54:37 +0500

    feat(mw): create middleware to normalize validation errors

---
+++ src/Movies.Api/Core/Middleware/ValidationMappingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+using FluentValidation;
+using Movies.Contracts.Responses;
+
+namespace Movies.Api.Core.Middleware;
+
+/// <summary>
+/// Server middleware to normalize the validation errors caused by FluentValidation
+/// </summary>
+/// <param name="next">The middleware delegate</param>
+public class ValidationMappingMiddleware(RequestDelegate next) {
+  public async Task InvokeAsync(HttpContext context) {
+    try {
+      await next(context);
+    }
+    catch (ValidationException ex) {
+      context.Response.StatusCode = (int)(ex.Data[ValidationHelper.StatusCodeKey] ?? 400);
+
+      ValidationFailureResponse validationFailureResponse = new() {
+        Message = ex.Message,
+        ErrorCode = (string?)(ex.Data[ValidationHelper.ErrorCodeKey] ?? null),
+        Errors = ex.Errors.Select(x => new ValidationResponse {
+          PropertyName = x.PropertyName,
+          Message = x.ErrorMessage,
+          AttemptedValue = x.AttemptedValue,
+          ErrorCode = x.ErrorCode,
+        })
+      };
+
+      await context.Response.WriteAsJsonAsync(validationFailureResponse.ToJson());
+    }
+  }
+}

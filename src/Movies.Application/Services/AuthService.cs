d7d505705ba8e9d9c58aad5631896294aa5d56e9
Junaid Atari <mj.atari@gmail.com>
2025-03-10 19:05:25 +0500

    feat(service): implement various methods, code cleanup, extend with base service class

---
+++ src/Movies.Application/Services/AuthService.cs
@@ -0,0 +1,109 @@
+// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+// See: https://www.telerik.com/blogs/asp-net-core-basics-authentication-authorization-jwt
+
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Movies.Application.Config;
+using Movies.Application.Core.Bases;
+using Movies.Application.Models;
+
+namespace Movies.Application.Services;
+
+public class AuthService(JwtConfiguration config) : ServiceBase {
+  /// <summary>
+  /// GeneratesJWT token against the given user
+  /// </summary>
+  /// <param name="user">The user model instance</param>
+  /// <param name="options">Additional options to customize the token</param>
+  /// <returns>The generated Json Web Token</returns>
+  public TokenResult GenerateToken(User user, JwtOptions? options = null) {
+    JwtSecurityTokenHandler tokenHandler = new();
+
+    SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(config.Key));
+
+    var issuedAt = DateTime.UtcNow;
+    var expires = DateTime.UtcNow.AddHours(options?.ExpirationInHours ?? config.ExpirationInHours);
+
+    SecurityTokenDescriptor tokenDescriptor = new() {
+      Subject = GenerateClaims(user, options),
+      Expires = expires,
+      Issuer = options?.Issuer ?? config.Issuer,
+      IssuedAt = issuedAt,
+      Audience = options?.Audience ?? config.Audience,
+      SigningCredentials = new(
+        securityKey, SecurityAlgorithms.HmacSha384Signature
+      )
+    };
+
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+
+    return new() {
+      Token = tokenHandler.WriteToken(token),
+      IssuedAt = issuedAt,
+      Expires = expires,
+    };
+  }
+
+  /// <summary>
+  /// Generate the token claims
+  /// </summary>
+  /// <param name="user">The user</param>
+  /// <param name="options">Additional options to customize the token</param>
+  /// <returns>The generated claims list</returns>
+  private static ClaimsIdentity GenerateClaims(User user, JwtOptions? options = null) {
+    ClaimsIdentity claims = new();
+
+    claims.AddClaims([
+      new Claim(JwtRegisteredClaimNames.Jti, user.AuthKey),
+      new Claim(JwtRegisteredClaimNames.Sub, "auth"),
+      new Claim(ClaimTypes.Role, user.Role),
+    ]);
+
+    if (options is null) return claims;
+    foreach (var claim in options.Value.Claims)
+      claims.AddClaim(claim);
+
+    return claims;
+  }
+}
+
+/// <summary>
+/// AuthPolicies represents the JWT role policies
+/// <p>Check <see cref="AuthService"/> class for usage.</p>
+/// </summary>
+public static class AuthPolicies {
+  /// <summary>
+  /// Applicable to `Admin` role
+  /// </summary>
+  public const string AdminPolicy = "AdminPolicy";
+
+  /// <summary>
+  /// Applicable to `User` role
+  /// </summary>
+  public const string UserPolicy = "UserPolicy";
+
+  /// <summary>
+  /// Applicable to signed-in users
+  /// </summary>
+  public const string AuthPolicy = "AuthPolicy";
+}
+
+public struct TokenResult {
+  public required string Token { get; init; }
+  public DateTime IssuedAt { get; init; }
+  public DateTime Expires { get; init; }
+}
+
+/// <summary>
+/// Additional JWT options to customize the token
+/// </summary>
+public struct JwtOptions {
+  public string Issuer { get; init; }
+  public string Audience { get; init; }
+  public int ExpirationInHours { get; init; }
+  public readonly IList<Claim> Claims => [];
+}
--- src/Movies.Application/Services/MovieService.cs
+++ src/Movies.Application/Services/MovieService.cs
@@ -3,48 +3,217 @@
 // Repository: https://github.com/blacksmoke26/csharp-webapp
 
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Movies.Application.Core.Bases;
+using Movies.Application.Database;
+using Movies.Application.Domain.Model;
+using Movies.Application.Helpers;
 using Movies.Application.Models;
 using Movies.Application.Repositories;
 
 namespace Movies.Application.Services;
 
-public class MovieService(IMovieRepository movieRepository, IValidator<Movie> movieValidator) : IMovieService {
-  /// <inheritdoc/>
-  public async Task<bool> CreateAsync(Movie movie, CancellationToken token = default) {
-    await movieValidator.ValidateAndThrowAsync(movie, cancellationToken: token);
-    return await movieRepository.CreateAsync(movie, token);
-  }
+public class MovieService(
+  DatabaseContext dbContext,
+  MovieRepository movieRepo,
+  IValidator<MovieCreateModel> createValidator,
+  IValidator<MovieUpdateModel> updateValidator): ServiceBase {
+  /// <summary>
+  /// Creates a movie
+  /// </summary>
+  /// <param name="input">The input object</param>
+  /// <param name="token">The cancellation token</param>
+  /// <returns>Whatever the movie is created or not</returns>
+  public async Task<Movie?> CreateAsync(
+    MovieCreateModel input, CancellationToken token = default) {
+    await createValidator.ValidateAndThrowAsync(input, token);
 
-  /// <inheritdoc/>
-  public Task<Movie?> GetByIdAsync(long id, CancellationToken token = default) {
-    return movieRepository.GetByIdAsync(id, token);
-  }
+    Movie movie = new() {
+      UserId = input.UserId,
+      Title = input.Title,
+      YearOfRelease = input.YearOfRelease
+    };
 
-  /// <inheritdoc/>
-  public Task<Movie?> GetBySlugAsync(string slug, CancellationToken token = default) {
-    return movieRepository.GetBySlugAsync(slug, token);
-  }
+    await using var transaction = await dbContext.Database.BeginTransactionAsync(token);
 
-  /// <inheritdoc/>
-  public Task<IEnumerable<Movie>> GetAllAsync(CancellationToken token = default) {
-    return movieRepository.GetAllAsync(token);
-  }
+    try {
+      movieRepo.GetDataSet().Add(movie);
+      await dbContext.SaveChangesAsync(token);
 
-  /// <inheritdoc/>
-  public async Task<Movie?> UpdateAsync(Movie movie, CancellationToken token = default) {
-    await movieValidator.ValidateAndThrowAsync(movie, cancellationToken: token);
+      // create new genres
+      dbContext.Genres.AddRange(
+        input.Genres.Select(name => new Genre {
+          MovieId = movie.Id,
+          Name = name
+        })
+      );
 
-    if (movie.Id is null) return null;
+      await dbContext.SaveChangesAsync(token);
+      await transaction.CommitAsync(token);
+    }
+    catch (Exception e) {
+      Console.WriteLine(e);
+      return null;
+    }
 
-    var exists = await movieRepository.ExistsByIdAsync((long)movie.Id, token);
-    if (!exists) return null;
-
-    await movieRepository.UpdateAsync(movie, token);
     return movie;
   }
 
-  /// <inheritdoc/>
-  public Task<bool> DeleteByIdAsync(long id, CancellationToken token = default) {
-    return movieRepository.DeleteByIdAsync(id, token);
+  /// <summary>
+  /// Updates a movie
+  /// </summary>
+  /// <param name="input">The input data object</param>
+  /// <param name="token">The cancellation token</param>
+  /// <returns>Whatever the movie is updated or not</returns>
+  /// <exception cref="ValidationException">When the inputs validation failed</exception>
+  public async Task<Movie?> UpdateAsync(MovieUpdateModel input, CancellationToken token = default) {
+    await updateValidator.ValidateAndThrowAsync(input, token);
+    var movie = await GetByUserIdAndPkAsync(input.UserId, input.Id, token);
+
+    await using var transaction = await dbContext.Database.BeginTransactionAsync(token);
+
+    try {
+      dbContext.Movies.Add(movie);
+      await dbContext.SaveChangesAsync(token);
+
+      // remove existing genres
+      await dbContext.Genres.Where(x => x.MovieId == movie.Id).ExecuteDeleteAsync(token);
+
+      // create new genres
+      dbContext.Genres.AddRange(
+        input.Genres.Select(name => new Genre {
+          MovieId = movie.Id,
+          Name = name
+        })
+      );
+
+      await dbContext.SaveChangesAsync(token);
+      await transaction.CommitAsync(token);
+    }
+    catch (Exception e) {
+      Console.WriteLine(e);
+      return null;
+    }
+
+    return movie;
   }
+
+  /// <summary>
+  /// Fetch the movie by the primary key
+  /// </summary>
+  /// <param name="id">Movie ID</param>
+  /// <param name="token">The cancellation token</param>
+  /// <returns>The fetched object, otherwise null if not found</returns>
+  /// <exception cref="ValidationException">When there is no record found</exception>
+  public Task<Movie> GetByPkAsync(long id, CancellationToken token = default) {
+    return GetOneAsync(x => x.Id == id, token);
+  }
+
+  /// <summary>
+  /// Fetch the movie by the slug
+  /// </summary>
+  /// <param name="slug">Movie slug</param>
+  /// <param name="token">The cancellation token</param>
+  /// <returns>The fetched object, otherwise null if not found</returns>  /// <exception cref="ValidationException">When there is no record found</exception>
+  public Task<Movie> GetBySlugAsync(string slug, CancellationToken token = default) {
+    return GetOneAsync(x => x.Slug == slug, token);
+  }
+
+  /// <summary>
+  /// Fetch the movie by the given owner user id and movie id
+  /// </summary>
+  /// <param name="userId">The user id</param>
+  /// <param name="movieId">The movie id</param>
+  /// <param name="token">The cancellation token</param>
+  /// <returns>The fetched object, otherwise null if not found</returns>
+  /// <exception cref="ValidationException">When there is no record found</exception>
+  public Task<Movie> GetByUserIdAndPkAsync(
+    long userId, long movieId, CancellationToken token = default) {
+    return GetOneAsync(x => x.UserId == userId && x.Id == movieId, token);
+  }
+
+  /// <inheritdoc cref="MovieRepository.GetOneAsync"/>
+  public async Task<Movie> GetOneAsync(Func<Movie, bool> whereFn, CancellationToken token = default) {
+    var movie = await movieRepo.GetOneAsync(whereFn, token);
+
+    if (movie is not null) return movie;
+
+    throw ValidationHelper.Create([
+      new() {
+        ErrorMessage = "Movie not found"
+      }
+    ], 404, "NOT_FOUND");
+  }
+
+  /// <inheritdoc cref="MovieRepository.GetManyAsync"/>
+  public Task<List<Movie>> GetAllAsync(Func<Movie, bool>? whereFn = null, CancellationToken token = default) {
+    return movieRepo.GetManyAsync(whereFn, token);
+  }
+
+  /// <inheritdoc cref="MovieRepository.DeleteAsync"/>
+  public Task<int> DeleteAsync(Func<Movie, bool> whereFn, CancellationToken token = default) {
+    return movieRepo.DeleteAsync(whereFn, token);
+  }
+
+  /// <inheritdoc cref="MovieRepository.ExistsAsync"/>
+  public Task<bool> ExistsAsync(Func<Movie, bool> whereFn, CancellationToken token = default) {
+    return movieRepo.ExistsAsync(whereFn.Invoke, token);
+  }
+
+  /// <summary>
+  /// Removes the movie by the given ID ad user id
+  /// </summary>
+  /// <param name="userId">The user id</param>
+  /// <param name="movieId">The movie id</param>
+  /// <param name="token">The cancellation token</param>
+  /// <returns>True if deleted, otherwise false</returns>
+  public async Task<bool> DeleteByUserAndPkAsync(long userId, long movieId, CancellationToken token = default) {
+    return await DeleteAsync(x => x.UserId == userId && x.Id == movieId, token) > 0;
+  }
+
+  /*/// <summary>
+  /// Fetch the movie by the given owner user id and slug
+  /// </summary>
+  /// <param name="userId">The user id</param>
+  /// <param name="slug">Movie Slug</param>
+  /// <param name="token">The cancellation token</param>
+  /// <returns>The fetched object, otherwise null if not found</returns>
+  /// <exception cref="ValidationException">When there is no record found</exception>
+  public Task<Movie> GetByUserIdAndSlugAsync(
+    long userId, string slug, CancellationToken token = default) {
+    return GetOneAsync(x => x.UserId == userId && x.Slug == slug, token);
+  }
+
+  /// <summary>
+  /// Returns all the movies by owner user id
+  /// </summary>
+  /// <param name="userId">The user id</param>
+  /// <param name="token">The cancellation token</param>
+  /// <returns>The fetched movies</returns>
+  public Task<List<Movie>> GetAllByUserAsync(long userId, CancellationToken token = default) {
+    return movieRepo.GetManyAsync(x => x.UserId == userId, token);
+  }
+  
+  /// <summary>
+  /// Removes the movie by the given ID ad user id
+  /// </summary>
+  /// <param name="id">The movie id</param>
+  /// <param name="token">The cancellation token</param>
+  /// <returns>True if deleted, otherwise false</returns>
+  public async Task<bool> DeleteByPkAsync(long id, CancellationToken token = default) {
+    return await DeleteAsync(x => x.Id == id, token) > 0;
+  }
+
+  /// <summary>
+  /// Verify that the given title exists against year of release or not
+  /// </summary>
+  /// <param name="title">The movie title</param>
+  /// <param name="yearOfRelease">Year of the release</param>
+  /// <param name="token">The cancellation token</param>
+  /// <returns>Whatever the movie exists</returns>
+  public Task<bool> TitleByYearExistsAsync(
+    string title, short yearOfRelease, CancellationToken token = default
+  ) {
+    return ExistsAsync(x => x.Title == title && x.YearOfRelease == yearOfRelease, token);
+  }*/
 }

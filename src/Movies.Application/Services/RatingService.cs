848b96b8e601d841d5ba8a281bf5ce772e112930
Junaid Atari <mj.atari@gmail.com>
2025-03-10 22:49:02 +0500

    feat(service): create service class

---
+++ src/Movies.Application/Services/RatingService.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
+using Movies.Application.Core.Bases;
+using Movies.Application.Core.Interfaces;
+using Movies.Application.Repositories;
+
+namespace Movies.Application.Services;
+
+public class RatingService(RatingRepository ratingRepo)
+  : ServiceBase, IServiceRepoInstance<RatingRepository> {
+  /// <inheritdoc/>
+  public RatingRepository GetRepo() => ratingRepo;
+}

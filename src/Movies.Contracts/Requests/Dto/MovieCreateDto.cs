5e59e5d64cffc06e34bce63e65d1ee06d9e8ea33
Junaid Atari <mj.atari@gmail.com>
2025-03-10 18:50:22 +0500

    feat(dto): create request DTOs

---
+++ src/Movies.Contracts/Requests/Dto/MovieCreateDto.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+namespace Movies.Contracts.Requests.Dto;
+
+public struct MovieCreateDto {
+  /// <summary>
+  /// The title of movie
+  /// </summary>
+  public string Title { get; set; }
+
+  /// <summary>
+  /// Official year of release
+  /// </summary>
+  public short YearOfRelease { get; set; }
+
+  /// <summary>
+  /// The list of genres
+  /// </summary>
+  public IEnumerable<string> Genres { get; set; }
+}

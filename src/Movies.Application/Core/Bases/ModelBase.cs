91c644868f3e60a9da2eb847bfd3a1fffae9e1c1
Junaid Atari <mj.atari@gmail.com>
2025-03-10 19:08:45 +0500

    feat(core): create base classes and interfaces

---
+++ src/Movies.Application/Core/Bases/ModelBase.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+// See also: [Entity Framework Core Generator](https://efg.loresoft.com/)
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Movies.Application.Core.Bases;
+
+/// <summary>
+/// ModelBase represents the parent class of all model classes
+/// </summary>
+public abstract class ModelBase {
+  /// <summary>
+  /// A callback function which triggers while db.SaveChanges() calls
+  /// </summary>
+  /// <param name="state">The changes state</param>
+  /// <param name="cancellationToken">The cancellation token</param>
+  /// <seealso cref="DbContext.SaveChangesAsync(System.Threading.CancellationToken)"/>
+  /// <seealso href="https://mbarkt3sto.hashnode.dev/how-to-auto-update-created-updated-and-deleted-timestamps-in-entity-framework-core">
+  /// How to Auto Update Created, Updated and Deleted Timestamps in Entity Framework Core?</seealso>
+  /// <returns>The results</returns>
+  public virtual Task OnTrackChangesAsync(
+    EntityState state, CancellationToken cancellationToken = default) {
+    return Task.CompletedTask;
+  }
+}
---
+++ src/Movies.Application/Core/Bases/ServiceBase.cs
@@ -0,0 +1,8 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
+namespace Movies.Application.Core.Bases;
+
+public abstract class ServiceBase {
+}

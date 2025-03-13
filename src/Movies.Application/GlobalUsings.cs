c8ed568a1e2f59895203b792598368b25fe58cab
Junaid Atari <mj.atari@gmail.com>
2025-03-13 23:32:46 +0500

    feat: register namespaces globally

---
+++ src/Movies.Application/GlobalUsings.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+// Global using directives
+
+global using System.Linq.Expressions;
+global using System.Text;
+global using FluentValidation;
+global using Microsoft.EntityFrameworkCore;
+global using Movies.Application.Config;
+global using Movies.Application.Core.Bases;
+global using Movies.Application.Database;
+global using Movies.Application.Domain.Model;
+global using Movies.Application.Helpers;
+global using Movies.Application.Models;
+global using Movies.Application.Repositories;
+global using Movies.Contracts.Requests.Dto;

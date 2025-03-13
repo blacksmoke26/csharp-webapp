e8ab2fa129df4970945def53f818f17b7f900cd5
Junaid Atari <mj.atari@gmail.com>
2025-03-13 23:38:07 +0500

    refactor: cleanup due to the globally available namespaces

--- src/Movies.Application/Repositories/RatingRepository.cs
+++ src/Movies.Application/Repositories/RatingRepository.cs
@@ -2,17 +2,10 @@
 // Copyright (c) 2025 Junaid Atari, and contributors
 // Repository:https://github.com/blacksmoke26/csharp-webapp
 
-using Microsoft.EntityFrameworkCore;
-using Movies.Application.Core.Bases;
-using Movies.Application.Database;
-using Movies.Application.Models;
-
 namespace Movies.Application.Repositories;
 
 public class RatingRepository(
-  DatabaseContext dbContext,
-  UserRepository userRepo,
-  MovieRepository movieRepo
+  DatabaseContext dbContext
 ) : RepositoryBase<Rating> {
   /// <inheritdoc/>
   public override DatabaseContext GetDbContext() => dbContext;

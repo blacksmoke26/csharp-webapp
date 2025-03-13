12924642253e381875b2f75a600fa17c533ae2ee
Junaid Atari <mj.atari@gmail.com>
2025-03-13 23:23:52 +0500

    refactor: cleanup due to the globally available namespaces

--- src/Movies.Application/Core/Interfaces/IRepositoryDbContext.cs
+++ src/Movies.Application/Core/Interfaces/IRepositoryDbContext.cs
@@ -2,10 +2,6 @@
 // Copyright (c) 2025 Junaid Atari, and contributors
 // Repository:https://github.com/blacksmoke26/csharp-webapp
 
-using Microsoft.EntityFrameworkCore;
-using Movies.Application.Core.Bases;
-using Movies.Application.Database;
-
 namespace Movies.Application.Core.Interfaces;
 
 public interface IRepositoryDbContext<TModel> where TModel : ModelBase {

7a1fb2106163be864a0529418fc32626f6c2203d
Junaid Atari <mj.atari@gmail.com>
2025-03-14 18:04:00 +0500

    refactor: extract structs, mapped structs using generic `Get` method

--- src/Movies.Application/Config/AppConfiguration.cs
+++ src/Movies.Application/Config/AppConfiguration.cs
@@ -6,48 +6,37 @@ using Microsoft.Extensions.Configuration;
 
 namespace Movies.Application.Config;
 
+/// <summary>
+/// AppConfiguration represents the application configuration retrieval
+/// </summary>
+/// <param name="config">Application configuration instance</param>
 public class AppConfiguration(IConfiguration config) {
-  public IConfiguration Config { get; } = config;
+  /// <summary>
+  /// Returns the IConfiguration instance
+  /// </summary>
+  /// <returns>The IConfiguration instance</returns>
+  public IConfiguration GetConfig() => config;
 
   /// <summary>
   /// Get JWT specific configuration
   /// <see cref="JwtConfiguration"/>
   /// </summary>
-  public JwtConfiguration JwtConfig() => new() {
-    Key = Config["Jwt:Key"] ?? string.Empty,
-    Issuer = Config["Jwt:Issuer"] ?? string.Empty,
-    Audience = Config["Jwt:Audience"] ?? string.Empty,
-    ExpirationInHours = int.Parse(Config["Jwt:ExpirationInHours"] ?? "8")
-  };
+  public JwtConfiguration JwtConfig()
+    => config.GetRequiredSection("Jwt").Get<JwtConfiguration>();
 
   /// <summary>
-  /// Get Authentication configuration
-  /// <see cref="Authentication"/>
+  /// Retrieve the authentication configuration from application configuration
+  /// <see cref="AuthenticationConfiguration"/>
   /// </summary>
-  public Authentication AuthConfig() => new() {
-    ExpireTokenAfterLogout =
-      bool.Parse(Config["Authentication:ExpireTokenAfterLogout"] ?? "false")
-  };
-}
+  /// <returns>The authentication configuration</returns>
+  public AuthenticationConfiguration AuthConfig()
+    => config.GetRequiredSection("Authentication").Get<AuthenticationConfiguration>();
 
-public struct Authentication {
-  public bool ExpireTokenAfterLogout { get; init; }
-}
-
-/// <summary>
-/// JwtConfiguration presents the options required by JWT Auth service
-/// <p>Check <see cref="AppConfiguration"/> class for usage.</p>
-/// </summary>
-public struct JwtConfiguration {
-  /// <summary>Represents a symmetric security key.</summary>
-  public required string Key { get; init; }
-
-  /// <summary>identifies the principal that issued the JWT</summary>
-  public required string Issuer { get; init; }
-
-  /// <summary>Identifies the recipients that the JWT is intended for.</summary>
-  public required string Audience { get; init; }
-
-  /// <summary>The expiration time in hours  on or after which the JWT MUST NOT be accepted for processing</summary>
-  public required int ExpirationInHours { get; init; }
+  /// <summary>
+  /// Retrieve the database configuration from application configuration
+  /// <see cref="DatabaseConfiguration"/>
+  /// </summary>
+  /// <returns>The database configuration</returns>
+  public DatabaseConfiguration DbConfig()
+    => config.GetRequiredSection("Database").Get<DatabaseConfiguration>();
 }

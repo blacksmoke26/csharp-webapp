f5137f159fd1a254f03d43371fc22371aa82b2e4
Junaid Atari <mj.atari@gmail.com>
2025-03-14 18:05:22 +0500

    refactor: suppress HTTPs warning

--- src/Movies.Api/Program.cs
+++ src/Movies.Api/Program.cs
@@ -17,19 +17,8 @@ if (app.Environment.IsDevelopment()) {
   //app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
-
-/*app.MapGet("/test", async (DatabaseContext dbContext) => {
-  /*var user = await dbContext.Users.Where(x => x.Id == 28)
-    .Select(x => new {
-      x.Email
-    }).FirstOrDefaultAsync();#1#
-
-  var user = await dbContext.Users.Where(x => x.Id == 282)
-    .Select(x => x.Email).FirstOrDefaultAsync();
-  return new {
-    user
-  };
-});*/
+if (!app.Environment.IsDevelopment()) {
+  app.UseHttpsRedirection();
+}
 
 app.Run();

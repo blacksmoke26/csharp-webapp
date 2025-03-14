c91b0e502fdafa544907d963864b8830977b3074
Junaid Atari <mj.atari@gmail.com>
2025-03-14 18:10:50 +0500

    feat(dto): create requests/responses DTOs

---
+++ src/Movies.Contracts/Requests/Dto/MovieRatingDto.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+namespace Movies.Contracts.Requests.Dto;
+
+public struct MovieRatingDto {
+  /// <summary>Movie ID</summary>
+  public short Rating { get; init; }
+
+  /// <summary>Movie ID</summary>
+  public string? Feedback { get; init; }
+}
---
+++ src/Movies.Contracts/Responses/Ratings/RatingResponse.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+namespace Movies.Contracts.Responses.Ratings;
+
+public struct RatingResponse {
+  public long? Id { get; set; }
+  public long? UserId { get; set; }
+  public long? MovieId { get; set; }
+  public short? Score { get; set; }
+  public string? Feedback { get; set; }
+  public DateTime? CreatedAt { get; set; }
+  public DateTime? UpdatedAt { get; set; }
+}

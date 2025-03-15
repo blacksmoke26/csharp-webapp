a9eb31f6c19ced2612798382d6427444b790c2d4
Junaid Atari <mj.atari@gmail.com>
2025-03-15 22:24:01 +0500

    feat(validator): create `Movie` get all query validator

---
+++ src/Movies.Api/Domain/Query/Validators/MoviesGetAllQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
+namespace Movies.Api.Domain.Query.Validators;
+
+public class MoviesGetAllQueryValidator: AbstractValidator<MoviesGetAllQuery> {
+  public MoviesGetAllQueryValidator() {
+    RuleFor(x => x.Title)
+      .MaximumLength(50);
+
+    RuleFor(x => x.Year)
+      .InclusiveBetween((short)1900, (short)DateTime.UtcNow.Year);
+  }
+}

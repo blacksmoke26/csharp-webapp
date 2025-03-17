c90fc3e3dd8d1937673831bda8d14163446bf37c
Junaid Atari <mj.atari@gmail.com>
2025-03-17 13:54:32 +0500

    refactor: update namespace

--- src/Movies.Api/Domain/Query/Validators/MoviesGetAllQueryValidator.cs
+++ src/Movies.Api/Domain/Query/Validators/MoviesGetAllQueryValidator.cs
@@ -1,8 +1,8 @@
 ﻿// Licensed to the end users under one or more agreements.
 // Copyright (c) 2025 Junaid Atari, and contributors
-// Website: https://github.com/blacksmoke26/
+// Repository:https://github.com/blacksmoke26/csharp-webapp
 
-using Movies.Api.Core.Validators;
+using Movies.Application.Requests.Validators;
 
 namespace Movies.Api.Domain.Query.Validators;
 

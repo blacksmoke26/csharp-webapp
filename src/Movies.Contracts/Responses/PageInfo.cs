ad538ac17a819c7c685c51821a69c845b67c3f96
Junaid Atari <mj.atari@gmail.com>
2025-03-17 13:37:22 +0500

    feat(dto): create paginated result response objects

---
+++ src/Movies.Contracts/Responses/PageInfo.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+namespace Movies.Contracts.Responses;
+
+public struct PageInfo {
+  public int CurrentPage { get; init; }
+  public int TotalPages { get; init; }
+  public bool HasPreviousPage { get; init; }
+  public bool HasNextPage { get; init; }
+}
---
+++ src/Movies.Contracts/Responses/PaginatedResult.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+namespace Movies.Contracts.Responses;
+
+public struct PaginatedResult {
+  public int CurrentPage { get; init; }
+  public int TotalPages { get; init; }
+  public int TotalCount { get; init; }
+  public bool HasPreviousPage { get; init; }
+  public bool HasNextPage { get; init; }
+  public IEnumerable<object> Rows { get; init; }
+}

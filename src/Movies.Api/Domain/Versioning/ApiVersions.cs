01826feaa9a346122e0f6e65f0f1a4c3ed31de64
Junaid Atari <mj.atari@gmail.com>
2025-03-17 23:27:16 +0500

    feat: create API versions list and service-collection extensions method

---
+++ src/Movies.Api/Domain/Versioning/ApiVersions.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
+namespace Movies.Api.Domain.Versioning;
+
+public static class ApiVersions {
+  public const string V10 = "1.0";
+  public const string V11 = "1.1";
+
+  public static ApiVersion FromText(string text)
+    => ApiVersionParser.Default.Parse(text);
+}

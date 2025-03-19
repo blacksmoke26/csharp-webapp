87f9df052558bf6d123c13802e983301774ec253
Junaid Atari <mj.atari@gmail.com>
2025-03-19 16:46:02 +0500

    refactor(core): move file, using `AddApiExplorer`

--- src/Movies.Api/Core/Versioning/ApiVersions.cs
+++ src/Movies.Api/Core/Versioning/ApiVersions.cs
@@ -1,8 +1,8 @@
 ﻿// Licensed to the end users under one or more agreements.
 // Copyright (c) 2025 Junaid Atari, and contributors
-// Website: https://github.com/blacksmoke26/
+// Repository:https://github.com/blacksmoke26/csharp-webapp
 
-namespace Movies.Api.Domain.Versioning;
+namespace Movies.Api.Core.Versioning;
 
 public static class ApiVersions {
   public const string V10 = "1.0";

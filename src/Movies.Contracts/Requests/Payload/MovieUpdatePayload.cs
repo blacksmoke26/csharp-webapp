26d2d63d802ee7b156cbfc2851f00c4640972d77
Junaid Atari <mj.atari@gmail.com>
2025-03-19 16:12:01 +0500

    feat(dto): move file, add swagger annotation

---
+++ src/Movies.Contracts/Requests/Payload/MovieUpdatePayload.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+namespace Movies.Contracts.Requests.Payload;
+
+[SwaggerSchema("Use to update a movie",
+  Required = ["Title", "YearOfRelease", "Genres"], WriteOnly = true)]
+public struct MovieUpdatePayload {
+  [Required] [DefaultValue("Face Off")] [SwaggerSchema("The title of movie", Nullable = false)]
+  public string Title { get; set; }
+
+  [Required] [DefaultValue("2025")] [SwaggerSchema("Official year of release", Nullable = false)]
+  public short YearOfRelease { get; set; }
+
+  [Required] [SwaggerSchema("The list of genres", Nullable = false)]
+  public IEnumerable<string> Genres { get; set; }
+}
---
+++ src/Movies.Contracts/Requests/Payload/UserLoginPayload.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+namespace Movies.Contracts.Requests.Payload;
+
+[SwaggerSchema("Use to login the existing user with credentials",
+  Required = ["Email", "Password"], WriteOnly = true)]
+public struct UserLoginCredentialPayload {
+  [SwaggerSchema("The email address", Format = "email", Nullable = false)] [Required] [DefaultValue("john.doe@example")]
+  public required string Email { get; init; }
+
+  [SwaggerSchema("The account password", Format = "password", Nullable = false)] [Required] [DefaultValue("<password>")]
+  public required string Password { get; init; }
+}
+
+public struct UserLoginClaimPayload {
+  public string Jti { get; init; }
+  public string Role { get; init; }
+}

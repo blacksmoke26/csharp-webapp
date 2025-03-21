1de05e13baf7b8183bb3cda477b44a7d1f0b3960
Junaid Atari <mj.atari@gmail.com>
2025-03-21 17:10:56 +0500

    feat(dto): create request specific payload DTO

---
+++ src/Movies.Contracts/Requests/Payload/Identity/PasswordResetRequestPayload.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+namespace Movies.Contracts.Requests.Payload.Identity;
+
+[SwaggerSchema("Use to send a request for a password reset", WriteOnly = true)]
+public struct PasswordResetRequestPayload {
+  [Required]
+  [DefaultValue("john.doe@domain.com")]
+  [SwaggerSchema("The email address", Format = "email", Nullable = false)]
+  public string Email { get; init; }
+}
---
+++ src/Movies.Contracts/Requests/Payload/User/ChangePasswordPayload.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+namespace Movies.Contracts.Requests.Payload.User;
+
+[SwaggerSchema("Use to change the account password", WriteOnly = true)]
+public struct ChangePasswordPayload {
+  [Required] [SwaggerSchema("The current password", Nullable = false)]
+  public string CurrentPassword { get; set; }
+
+  [Required] [SwaggerSchema("The new password to update", Nullable = false)]
+  public string NewPassword { get; set; }
+}

9b1bc3cfc8ec15bfa794b1b1eee8775d76f5d757
Junaid Atari <mj.atari@gmail.com>
2025-03-21 17:11:40 +0500

    feat: create error specific response DTO

---
+++ src/Movies.Contracts/Responses/OperationFailureResponse.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+namespace Movies.Contracts.Responses;
+
+[SwaggerSchema(
+  "This represents the error response caused by some operation failure", ReadOnly = true)]
+public struct OperationFailureResponse {
+  [SwaggerSchema("The error code")]
+  public required string ErrorCode { get; init; }
+
+  [SwaggerSchema("List of validation errors")]
+  public required IEnumerable<OperationFailureError> Errors { get; init; }
+}
+
+public struct OperationFailureError {
+  [SwaggerSchema("The error message")]
+  public required string Message { get; set; }
+}

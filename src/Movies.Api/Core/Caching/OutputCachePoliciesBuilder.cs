f9d113e60e62000d3278b64e7edc95be349e539f
Junaid Atari <mj.atari@gmail.com>
2025-03-22 15:54:41 +0500

    feat(cache): create class: implementing output caching

---
+++ src/Movies.Api/Core/Caching/OutputCachePoliciesBuilder.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+using Microsoft.AspNetCore.OutputCaching;
+
+namespace Movies.Api.Core.Caching;
+
+public static class OutputCachePoliciesBuilder {
+  /// <summary>
+  /// Builds the output caching policies
+  /// </summary>
+  /// <param name="opt">The <see cref="Microsoft.AspNetCore.OutputCaching.OutputCacheOptions">OutputCacheOptions</see> options</param>
+  public static void Build(OutputCacheOptions opt) {
+    // Add `Movies` policy
+    opt.AddPolicy(CachePolicies.Movies.Policy, c => {
+      c.Cache()
+        .Expire(TimeSpan.FromMinutes(1))
+        .SetVaryByQuery(CachePolicies.Movies.VaryByQuery)
+        .Tag(CachePolicies.Movies.Policy);
+    });
+  }
+}

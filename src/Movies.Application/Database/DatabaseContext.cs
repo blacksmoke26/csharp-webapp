415e4ceb2d8a789aeb2143ae15990378a3c7c7d5
Junaid Atari <mj.atari@gmail.com>
2025-03-22 11:10:22 +0500

    refactor(db): implement improvements regarding JSONB, add links

--- src/Movies.Application/Database/DatabaseContext.cs
+++ src/Movies.Application/Database/DatabaseContext.cs
@@ -4,7 +4,11 @@
 // See also: https://learn.microsoft.com/en-us/ef/core/modeling/entity-properties
 // See also: https://www.npgsql.org/efcore/modeling/generated-properties.html?tabs=13%2Cefcore5
 // See also: https://medium.com/@serhiikokhan/jsonb-in-postgresql-with-ef-core-cc945f1aba2a
+// See also: https://mbarkt3sto.hashnode.dev/ef-core-and-postgresql-working-with-json-data
+// See also: https://www.npgsql.org/efcore/mapping/translations.html
 
+using System.Globalization;
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Movies.Application.Database;
@@ -15,13 +19,16 @@ public class DatabaseContext : DbContext {
 
   /// <summary>Users entity object</summary>
   public DbSet<User> Users { get; set; }
+
   /// <summary>Movies entity object</summary>
   public DbSet<Movie> Movies { get; set; }
+
   /// <summary>Ratings entity object</summary>
   public DbSet<Rating> Ratings { get; set; }
+
   /// <summary>Genres entity object</summary>
   public DbSet<Genre> Genres { get; set; }
-  
+
   /// <inheritdoc cref="Microsoft.EntityFrameworkCore.Infrastructure.DatabaseFacade.BeginTransactionAsync"/>
   public DatabaseContext(DatabaseConfiguration config) {
     _config = config;
@@ -36,8 +43,7 @@ public class DatabaseContext : DbContext {
   /// <summary>Prints the query result to the console</summary>
   /// <param name="message">Message details</param>
   private void LogToConsole(string message) {
-    if (_config.Logging.Enabled)
-      Console.WriteLine(message);
+    if (_config.Logging.Enabled) Console.WriteLine(message);
   }
 
   /// <inheritdoc/>
@@ -45,17 +51,24 @@ public class DatabaseContext : DbContext {
     optionsBuilder
       .LogTo(LogToConsole, _config.Logging.LogLevel)
       .UseNpgsql(_config.ConnectionString, x =>
-        x.ConfigureDataSource(x
+        x.ConfigureDataSource(y => {
           // Parse JSON as dynamic Object
-          => x.EnableDynamicJson()
-        ))
+          // See: https://www.npgsql.org/doc/types/json.html?tabs=datasource
+          y.EnableDynamicJson();
+          y.ConfigureJsonOptions(new JsonSerializerOptions(JsonSerializerDefaults.Web));
+          y.EnableParameterLogging();
+        }))
       // use snake_care for table and columns name explicitly 
-      .UseSnakeCaseNamingConvention();
+      .UseSnakeCaseNamingConvention(CultureInfo.InvariantCulture);
   }
 
   /// <inheritdoc/>
   protected override void OnModelCreating(ModelBuilder modelBuilder) {
     modelBuilder.UseIdentityByDefaultColumns();
+    modelBuilder.Entity<User>(entity => {
+      entity.Property(b => b.Metadata)
+        .HasColumnType("jsonb"); // Use jsonb data type for PostgreSQL
+    });
   }
 
   /// <inheritdoc/>

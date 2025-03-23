7de47df8b607146d3068a486e6d0ccd46dde1e11
Junaid Atari <mj.atari@gmail.com>
2025-03-23 16:55:37 +0500

    feat(core): code restructured, using configurators

---
+++ src/Movies.Api/Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+using Movies.Api.Core.Configurators;
+using Movies.Application.Config;
+
+namespace Movies.Api.Core.Extensions;
+
+/// <summary>
+/// Represents as the part of dependency injection to register the dependencies inside the application
+/// </summary>
+public static class ServiceCollectionExtensions {
+  /// <summary>
+  /// Registers application level services 
+  /// </summary>
+  /// <param name="services">ServiceCollection instance</param>
+  /// <param name="configuration">Application configuration</param>
+  public static void InitBootstrapper(
+    this IServiceCollection services, IConfiguration configuration
+  ) {
+    var config = new AppConfiguration(configuration);
+
+    services.AddSingleton<AppConfiguration>(_ => config);
+
+    ApiVersioningConfigurator.Configure(services, config);
+
+    OutputCachingConfigurator.Configure(services, config);
+    ResponseCachingConfigurator.Configure(services, config);
+    
+    JwtAuthenticationConfigurator.Configure(services, config);
+    ErrorHandlerConfigurator.Configure(services, config);
+
+    ControllersConfigurator.Configure(services, config);
+    SwaggerConfigurator.Configure(services, config);
+
+    HealthCheckConfigurator.Configure(services, config);
+    RateLimiterConfigurator.Configure(services, config);
+
+    services.AddApplication();
+    
+    DatabaseConfigurator.Configure(services, config);
+
+    MiddlewareConfigurator.Configure(services, config);
+  }
+}
---
+++ src/Movies.Api/Core/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository:https://github.com/blacksmoke26/csharp-webapp
+
+using Microsoft.AspNetCore.HttpOverrides;
+using Movies.Api.Core.Configurators;
+
+namespace Movies.Api.Core.Extensions;
+
+public static class WebApplicationExtensions {
+  /// <summary>
+  /// Registers web application level services 
+  /// </summary>
+  /// <param name="app">WebApplication instance</param>
+  public static void UseBootstrapper(this WebApplication app) {
+    JwtAuthenticationConfigurator.Use(app);
+
+    app.UseForwardedHeaders(new ForwardedHeadersOptions {
+      ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+    });
+
+    RateLimiterConfigurator.Use(app);
+    HealthCheckConfigurator.Use(app);
+
+    //  Warning: Must be called before caching when using the CORS middleware.
+    //app.UseCors();
+
+    ResponseCachingConfigurator.Use(app);
+    OutputCachingConfigurator.Use(app);
+
+    SwaggerConfigurator.Use(app);
+
+    ErrorHandlerConfigurator.Use(app);
+    MiddlewareConfigurator.Use(app);
+
+    ControllersConfigurator.Use(app);
+  }
+}

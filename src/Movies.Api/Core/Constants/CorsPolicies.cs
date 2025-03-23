ac6259b87455c1d948fbdb23d415625e0359a2fa
Junaid Atari <mj.atari@gmail.com>
2025-03-23 22:34:40 +0500

    feat(configurator): implement `CORS` configurator

---
+++ src/Movies.Api/Core/Constants/CorsPolicies.cs
@@ -0,0 +1,9 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
+namespace Movies.Api.Core.Constants;
+
+public static class CorsPolicies {
+  public const string Default = "Default";
+}

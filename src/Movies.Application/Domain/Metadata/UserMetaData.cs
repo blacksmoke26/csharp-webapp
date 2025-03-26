67dbcfc138a97ae468678f4bcab1ee41e8ae1852
Junaid Atari <mj.atari@gmail.com>
2025-03-26 19:47:03 +0500

    feat(meta): implicitly declare json properties name

--- src/Movies.Application/Domain/Metadata/UserMetaData.cs
+++ src/Movies.Application/Domain/Metadata/UserMetaData.cs
@@ -10,28 +10,44 @@ namespace Movies.Application.Domain.Metadata;
 public record UserMetadata {
   [StringLength(10), JsonPropertyName("language")]
   public string Language { get; set; } = "en-US";
+
   [StringLength(20), JsonPropertyName("timezone")]
   public string Timezone { get; set; } = "UTC";
+
+  [JsonPropertyName("security")]
   public UserMetadataSecurity Security { get; set; } = new();
+
+  [JsonPropertyName("activation")]
   public UserMetadataActivation Activation { get; set; } = new();
+
+  [JsonPropertyName("password")]
   public UserMetadataPassword Password { get; set; } = new();
+
+  [JsonPropertyName("loggedInHistory")]
   public UserMetadataLoggedInHistory LoggedInHistory { get; set; } = new();
 }
 
 [Owned]
 public record UserMetadataActivation {
+  [JsonPropertyName("code")]
   public string? Code { get; set; }
+
+  [JsonPropertyName("pending")]
   public bool? Pending { get; set; }
+
+  [JsonPropertyName("requestedAt")]
   public DateTime? RequestedAt { get; set; }
+
+  [JsonPropertyName("completedAt")]
   public DateTime? CompletedAt { get; set; }
-  
+
   /// <summary>An event method which invoked which account successfully created</summary>
   public void OnSignedUp() {
     Pending = true;
     Code = IdentityHelper.GenerateActivationCode();
     RequestedAt = DateTime.UtcNow;
   }
-  
+
   /// <summary>An event method which invoked which account successfully verified</summary>
   public void OnActivated() {
     Pending = false;
@@ -42,10 +58,16 @@ public record UserMetadataActivation {
 
 [Owned]
 public record UserMetadataLoggedInHistory {
-  [MaxLength(20)]
+  [MaxLength(20)] [JsonPropertyName("lastIp")]
   public string? LastIp { get; set; } = null;
+
+  [JsonPropertyName("lastDate")]
   public DateTime? LastDate { get; set; }
+
+  [JsonPropertyName("successCount")]
   public int SuccessCount { get; set; } = 0;
+
+  [JsonPropertyName("failedCount")]
   public int FailedCount { get; set; } = 0;
 
   /// <summary>An event method which invoked upon successful authentication</summary>
@@ -60,12 +82,22 @@ public record UserMetadataLoggedInHistory {
 
 [Owned]
 public record UserMetadataPassword {
+  [JsonPropertyName("lastResetAt")]
   public DateTime? LastResetAt { get; set; }
-  [StringLength(IdentityHelper.PasswordResetCodeSize)]
+
+  [StringLength(IdentityHelper.PasswordResetCodeSize), JsonPropertyName("resetCode")]
   public string? ResetCode { get; set; }
+
+  [JsonPropertyName("resetCodeRequestedAt")]
   public DateTime? ResetCodeRequestedAt { get; set; }
+
+  [JsonPropertyName("resetCount")]
   public int ResetCount { get; set; }
+
+  [JsonPropertyName("lastUpdatedAt")]
   public DateTime? LastUpdatedAt { get; set; }
+
+  [JsonPropertyName("updatedCount")]
   public int UpdatedCount { get; set; }
 
   /// <summary>An event method which invoked upon password changing</summary>
@@ -98,5 +130,6 @@ public record UserMetadataPassword {
 
 [Owned]
 public record UserMetadataSecurity {
+  [JsonPropertyName("tokenInvalidate")]
   public bool TokenInvalidate { get; set; }
 }

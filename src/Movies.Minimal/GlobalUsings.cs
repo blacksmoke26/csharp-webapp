8b4ddc5d22196d7020bfdb4ae245dae7a4e9a20c
Junaid Atari <mj.atari@gmail.com>
2025-03-26 20:41:57 +0500

    feat: register namespaces globally

---
+++ src/Movies.Minimal/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// Global using directives
+
+global using Movies.Application.Helpers;
+global using Movies.Application.Repositories;
+global using Movies.Application.Services;
+global using Movies.Contracts.Requests.Payload;

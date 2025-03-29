2a82d10e7ab9ad01df02c9a3f9bd54e12f9e5f0a
Junaid Atari <mj.atari@gmail.com>
2025-03-29 23:21:26 +0500

    refactor: extract login to a configurator

--- src/Movies.Minimal/Program.cs
+++ src/Movies.Minimal/Program.cs
@@ -2,31 +2,20 @@
 // Copyright (c) 2025 Junaid Atari, and contributors
 // Repository:https://github.com/blacksmoke26/csharp-webapp
 // Guide: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis
-// Guide: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/openapi/include-metadata
-// Scalar: https://github.com/scalar/scalar/blob/main/documentation/integrations/dotnet.md
 
-using Movies.Minimal.Core.Extensions;
 using Movies.Minimal.Endpoints;
-using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.InitBootstrapper(builder.Configuration);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi(x => {
-  //x.ShouldInclude = description => description.GroupName == null || description.GroupName == x.DocumentName;
-});
-
 var app = builder.Build();
 
+app.CreateApiVersionSet(); 
 app.UseBootstrapper();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
-  app.MapOpenApi().AllowAnonymous();
-  app.MapScalarApiReference();
   //app.UseHttpsRedirection();
 }
 
